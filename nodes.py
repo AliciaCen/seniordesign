b29426ce636ab391931f69fdb903782603a4876b
Rippies <merrik.watson@yahoo.com>
2020-03-15 20:10:03 -0500

    changed checkConnections to print dict not items

--- nodes.py
+++ nodes.py
@@ -1,4 +1,4 @@
-#Imports, currently unused
+# Imports
 import sys
 import json
 
@@ -9,11 +9,11 @@ import json
 # removeVertex(node)				-> TODO (extra Data structure to hold nodes)
 # removeEdge(edge)					-> TODO node.disconnect(otherNode) or node.disconnectAll()
 
-#List of Nodes
+# List of Nodes
 dataList = []
 
-#Basic class structure. Port and Rate are intended to be integers
-#Wireless is a boolean set to 0 or 1 and Connections is a list
+# Basic class structure. Port and Rate are intended to be integers
+# Wireless is a boolean set to 0 or 1 and Connections is a list
 class Node:
 	def __init__(self, name, ports = 1, bitRate = 0, wireless = 0, connections = None):
 		self.name = name
@@ -25,17 +25,18 @@ class Node:
 	def __str__(self):
 		return "I am {}, a router with {} ports, a data rate of {}".format(self.name, self.ports, self.bitRate)
 
-	#Connect node to existing node
-	#Checks if nodes are already connected
+	# Connect node to existing node
+	# Checks if nodes are already connected
 	def connect(self, other):
 		if other.name not in self.connections:
 			self.connections.append(other.name)
 			other.connections.append(self.name)
 
-	#Reads all existing connections
+	# Reads all existing connections
 	def checkConnections(self):
-		for item in self.connections:
-			print(item)
+		print (self.connections)
+		# for item in self.connections:
+		# 	print(item)
 
 	# Check to see if the node has that neighbor
 	# If so, delete it
@@ -52,21 +53,21 @@ class Node:
 	def from_json(cls, data):
 		return cls(data)
 
-#Create 5 basic router nodes
+# Create 5 basic router nodes
 Router1 = Node('Router_1', 4, 2400, 0)
 Router2 = Node('Router_2', 5, 3000, 0)
 Router3 = Node('Router_3', 6, 3600, 0)
 Router4 = Node('Router_4', 7, 4200, 0)
 Router5 = Node('Router_5', 8, 4800, 0)
 
-#List of all nodes
+# List of all nodes
 dataList.append(Router1)
 dataList.append(Router2)
 dataList.append(Router3)
 dataList.append(Router4)
 dataList.append(Router5)
 
-#Test connections between routers
+# Test connections between routers
 Router1.connect(Router3)
 Router3.connect(Router1)
 Router1.connect(Router4)
