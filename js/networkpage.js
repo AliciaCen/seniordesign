f20092d47bd46ba24502b9d12ab82bbc6427bff9
Kerri Gustinger <ksg027@latech.edu>
2020-04-06 18:38:10 -0500

    Updating Network Configuration Modal

--- js/networkpage.js
+++ js/networkpage.js
@@ -7,5 +7,24 @@ $(function(){
 	$("#modalcontainer").load("networkmodal.html").hide();
 });
 $(function(){
-	$("#introModal").load("intromodal.html").hide();
+	$("#configModal").load("configmodal.html").hide();
+});
+function openForm() {
+	document.getElementById("networkConfig").style.display = "block";
+}
+function closeForm() {
+	document.getElementById("networkConfig").style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('openConfigBtn')
+		.addEventListener('click', openForm);
+});
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('closeConfigBtn')
+		.addEventListener('click', closeForm);
+});
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('startConfigBtn')
+		.addEventListener('click', closeForm);
 });
